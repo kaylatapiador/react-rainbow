9c6361484d8a5c7389acf5c5faa20d8107e3d1ec
kayla tapiador <kaylatapiador@yahoo.com>
2024-04-14 10:11:06 -0700

    completed work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm.js';
 
 function App() {
+
+  const addColor = (newColor) => {
+    setColors([...colors,newColor])
+  }
+
+  let [colors,setColors] = useState (['violet', 'blue', 'lightblue','green', 'greenyellow','yellow','red', 'orange' ])
+
+  let colorMap = colors.map((color,i) => {
+    return(
+      <ColorBlock key = {i} color = {color} />
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
-  );
+  )
 }
 
 export default App;
